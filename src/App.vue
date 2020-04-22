207eab872db3f3babb08e5f6fad4b6910f064b57
twaite <tim@dialexa.com>
2020-04-21 23:12:30 -0500

    Added svg with some optimization

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@
       class="text-black focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
     />
 
-    <state-county-chart class="mt-5" />
-
+    <!-- <state-county-chart class="mt-5" /> -->
+    <svg-county-chart class="mt-5" />
   </div>
 </template>
 
@@ -22,31 +22,34 @@
 import {
   computed,
   defineComponent,
-  ref,
   onMounted,
   reactive,
-} from "@vue/composition-api";
-import StateCountyChart from './components/state-county-chart.vue';
+} from '@vue/composition-api';
+import StateCountyChart from '@/components/state-county-chart.vue';
+import SvgCountyChart from '@/components/svg-county-chart.vue';
 
 type StateType = {
   zipCode?: number;
 };
 
 const IndexComponent = defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    'state-county-chart': StateCountyChart
+    'state-county-chart': StateCountyChart,
+    SvgCountyChart,
   },
   setup() {
     const state: StateType = reactive({
-      zipCode: 0
+      zipCode: 0,
       // double: computed(() => state.count * 2)
     });
 
-    const texas = computed(() => "Texas");
+    const texas = computed(() => 'Texas');
 
     async function go() {
-      console.log('Ok, tell all the charts to rerender with the new input, if it\'s a valid zip code...');
+      console.log(
+        "Ok, tell all the charts to rerender with the new input, if it's a valid zip code..."
+      );
     }
 
     onMounted(async () => {
---
+++ src/components/svg-county-chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="map-container">
+    <CountyMap />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import CountyMap from '@/components/counties';
+
+const SvgCountyChart = defineComponent({
+  components: {
+    CountyMap,
+  },
+});
+
+export default SvgCountyChart;
+</script>
+
+<style>
+.map-container {
+  @apply border border-gray-300 rounded bg-gray-700 p-2 shadow-md;
+}
+
+path:hover,
+path:active {
+  fill: red;
+  @apply: shadow;
+}
+
+path.no-hover:hover {
+  fill: none;
+}
+</style>
